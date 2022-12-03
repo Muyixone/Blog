d5c805d7f648bb9d5cb75be10fd68bf00cab3d4a
Osamuyi <muyixone@yahoo.com>
2022-12-03 13:11:17 +0100

    Adding a templating engine(Views) to the project

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const path = require('path');
 
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 const port = 3333;
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 app.get('/about', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  res.render('about');
 });
 app.get('/contact', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+  res.render('contact');
 });
 app.get('/post', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+  res.render('post');
 });
 
 app.listen(port, (req, res) => {
