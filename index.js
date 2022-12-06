704013ec03155908441993d129e5b9d856c88024
Osamuyi <muyixone@yahoo.com>
2022-12-06 23:51:31 +0100

    Saving a Blogpost to the database

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const path = require('path');
 
+const BlogPost = require('./models/blogpost');
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 const port = 3333;
@@ -23,7 +29,13 @@ app.get('/contact', (req, res, next) => {
 app.get('/post', (req, res, next) => {
   res.render('post');
 });
-
+app.get('/post/new', (req, res, next) => {
+  res.render('create');
+});
+app.post('/post/store', async (req, res, next) => {
+  let body = req.body;
+  await BlogPost.create(body);
+});
 app.listen(port, (req, res) => {
   console.log('Server connected on port', port);
 });
