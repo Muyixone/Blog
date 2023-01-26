e518bfdbbe24bb28cd33ab90ed59c4512ee6743e
Osamuyi <muyixone@yahoo.com>
2023-01-26 12:55:15 +0100

    Implemented the read_time function

--- controller/controller.createBlog.js
+++ controller/controller.createBlog.js
@@ -1,7 +1,21 @@
 const blogModel = require('../models/blogpost');
 
 exports.createBlogPost = async (req, res, next) => {
-  const blog = new blogModel({ ...req.body, author: req.user._id });
+  let { body } = req.body;
+  const wordsPerMinute = 200;
+  const noOfWords = body.split(/\s+/).length;
+  const minutes = noOfWords / wordsPerMinute;
+  const estimatedReadTime = Math.ceil(minutes);
+  const readTime =
+    estimatedReadTime > 1
+      ? `${estimatedReadTime} minutes read`
+      : `${estimatedReadTime} minute read`;
+
+  const blog = new blogModel({
+    ...req.body,
+    reading_time: readTime,
+    author: req.user._id,
+  });
 
   //save the blog post to the database
   await blog.save();
